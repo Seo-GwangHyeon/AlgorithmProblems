7ff31001123b6f80c1e0673297d066714b03c15f
SGH <sgh2122@naver.com>
2016-05-29 16:12:24 +0900

    큐 까지 만들었으나 큐의 Print가 안됨

--- Linked_PJ/Linked_PJ/Header.h
+++ Linked_PJ/Linked_PJ/Header.h
@@ -4,33 +4,53 @@
 using namespace std;
 
 template <class T>
-class LinkedStackNode;
+class LinkedNode;
 
 template <class T>
 class LinkedStack//연결리스트 스택 클래스
 {
-	friend class LinkedStackNode<T>;
+	friend class LinkedNode<T>;
 private:
-	LinkedStackNode<T> *top;//탑 원소 first 원소가 된다.
+	LinkedNode<T> *top;//탑 원소 first 원소가 된다.
 public:
 	LinkedStack<T>()
 	{
 		top = NULL;//처음 탑 값 NULL
 	}
 	void Push(T pushll);
+	void Pop() ;
+	bool IsEmpty() const;
+	void Print() const;
+};
+//----------------------------------------------------------
+template <class T>
+class LinkedQueue//연결리스트 큐 클래스
+{
+	friend class LinkedNode<T>;
+private:
+	LinkedNode<T> *front;
+	LinkedNode<T> *rear;
+public:
+	LinkedQueue<T>()
+	{
+		front = NULL;//처음 탑 값 NULL
+		rear = front;
+	}
+	void Push(T pushll);
 	void Pop();
-	bool IsEmpty();
-	void Print();
+	bool IsEmpty() const;
+	void Print() const;
 };
 
 template <class T>//탬플릿
-class LinkedStackNode//연결리스트 스택 노드 클래스
+class LinkedNode//연결리스트 스택 노드 클래스
 {
 	friend class LinkedStack<T>;
+	friend class LinkedQueue<T>;
 private:
 	T data;//탬플릿을 이용한 데이터
-	LinkedStackNode<T> *link;//다음 원소
-	LinkedStackNode(T data, LinkedStackNode<T> *link)//생성자 다음 노드와 데이터를 받음
+	LinkedNode<T> *link;//다음 원소
+	LinkedNode(T data, LinkedNode<T> *link)//생성자 다음 노드와 데이터를 받음
 	{
 		this->data = data;
 		this->link = link;
--- Linked_PJ/Linked_PJ/main.cpp
+++ Linked_PJ/Linked_PJ/main.cpp
@@ -1,29 +1,30 @@
 #include "Header.h"
+
 template <class T>
 void LinkedStack<T>::Push(T pushll)
 {
-	top = new LinkedStackNode<T>(pushll, top);//새 탑을 만들어 기존탑을 다음 노드로 넘긴다
+	top = new LinkedNode<T>(pushll, top);//새 탑을 만들어 기존탑을 다음 노드로 넘긴다
 }
 
 template <class T>
-void LinkedStack<T>::Pop()
+void LinkedStack<T>::Pop() 
 {
 	if (IsEmpty()) throw "Stack is empty. Cannot delete.";
-	LinkedStackNode<T> *delNode = top;
+	LinkedNode<T> *delNode = top;
 	top = top->link;//탑을 빼고 기존 탑의 다음원소를 탑으로 변경한다.
 	delete delNode;//탑을 해제한다.
 }
 
 template <class T>
-bool LinkedStack<T>::IsEmpty()// 비어있으면 true값을 출력한다.
+bool LinkedStack<T>::IsEmpty() const// 비어있으면 true값을 출력한다.
 {
 	return top == NULL;
 }
 
 template <class T>
-void LinkedStack<T>::Print()//전체 원소를 출력한다
+void LinkedStack<T>::Print() const//전체 원소를 출력한다
 {
-	LinkedStackNode<T> *temp = top;
+	LinkedNode<T> *temp = top;
 	while (temp)
 	{
 		cout << temp->data << " ";
@@ -31,8 +32,49 @@ void LinkedStack<T>::Print()//전체 원소를 출력한다
 	}
 	cout << endl;
 }
+//---------------------------------------------------------------------
 
+template <class T>
+void LinkedQueue<T>::Push(T pushll)
+{
+	if (this->IsEmpty())
+	{
+		this->front = this->rear = new LinkedNode<T>(pushll, NULL);
+	}
+	else
+	{
+		this->rear->link = new LinkedNode<T>(pushll, NULL);//기존 rear의 링크를 새 rear로 설정하고 다음 값이 널 인 새 rear를 연결한다.
+		this->rear = this->rear->link;
 
+	}
+}
+
+template <class T>
+void LinkedQueue<T>::Pop()
+{
+	if (IsEmpty()) throw "Queue is empty. Cannot delete.";
+	LinkedNode<T> *delNode = front;
+	front = front->link;//탑을 빼고 기존 탑의 다음원소를 탑으로 변경한다.
+	delete delNode;//탑을 해제한다.
+}
+
+template <class T>
+bool LinkedQueue<T>::IsEmpty() const// 비어있으면 true값을 출력한다.
+{
+	return front == rear;
+}
+
+template <class T>// 전체 원소 출력
+void LinkedQueue<T>::Print() const
+{
+	LinkedNode<T> *temp = this->front;
+	while (temp ==NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->link;
+	}
+	cout << endl;
+}
 
 int main()
 {
@@ -46,6 +88,18 @@ int main()
 	{
 		A.Pop();
 		A.Print();
+	}
+	LinkedQueue<int> b;
+	for (int i = 0; i<15; i++)
+	{
+		b.Push(i + 1);
+		b.Print();
+	}
+	//b.Print();
+	for (int i = 0; i<14; i++)
+	{
+		b.Pop();
+		b.Print();
 	}
 
 
