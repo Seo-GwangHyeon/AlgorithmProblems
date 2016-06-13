49cd3781725d1f1eb0a5eed6bf8e966364ec3068
SGH <sgh2122@naver.com>
2016-06-13 22:13:31 +0900

    점프게임 기본 폼 완성

--- JumpGame/JumpGame/JumpGame.cpp
+++ JumpGame/JumpGame/JumpGame.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main()
 {
-	int count = 0;
-	ifstream fin()
-	cin >> count;
-
+	int count = 0, size=0;
+	int matrix[100][100] = { 0 };
+	int result[50] = { 0 };
+	ifstream fin;
+	fin.open("input.txt");
+	fin >> count;
+	fin >> size;
 	for (int i = 0; i < count; i++)
 	{
+		for (int j = 0; j < size; j++)
+		{
+			for (int m = 0; m < size; m++)
+			{
+				fin >> matrix[j][m];
+			}
+		}
 
+		//여기서 결과 입력
 	}
+
+
+
 }
